11dc820f5ca419b101940b969ff8b77b671fdb6d
muslimipro <muslim.ipro@gmail.com>
2021-02-25 12:05:24 +0600

    Fixtures card changed

--- lib/screens/fixtures.dart
+++ lib/screens/fixtures.dart
@@ -35,93 +35,95 @@ class MatchesListItem extends StatelessWidget {
       margin: EdgeInsets.all(20.0),
       // padding: EdgeInsets.all(8.0),
       color: Colors.blueGrey[50],
-      height: 300,
+      height: 250,
       child: Stack(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  match.league,
-                  style: TextStyle(
-                    fontSize: 23.0,
-                    color: appTheme.primaryColor,
-                    fontWeight: FontWeight.w800,
-                  ),
-                ),
-                SizedBox(height: 20),
-                Text(match.arena),
-                SizedBox(height: 10),
-                Text('24 Feb 2021, 2:00 AM'),
-                SizedBox(height: 20),
-                Padding(
-                  padding: const EdgeInsets.only(right: 30),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        children: [
-                          Row(
-                            children: [
-                              Image(
-                                image:
-                                    AssetImage('assets/logos/chelsea-fc.png'),
-                                width: 50,
-                                height: 50,
-                              ),
-                              SizedBox(width: 10),
-                              Text('Chelsea'),
-                            ],
-                          ),
-                          SizedBox(height: 10),
-                          Row(
-                            children: [
-                              Image(
-                                image:
-                                    AssetImage('assets/logos/arsenal-fc.png'),
-                                width: 50,
-                                height: 50,
-                              ),
-                              SizedBox(width: 10),
-                              Text('Arsenal'),
-                            ],
-                          )
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          Text(
-                            '24',
-                            style: TextStyle(
-                              fontSize: 23.0,
-                              color: appTheme.primaryColor,
-                              fontWeight: FontWeight.w900,
-                            ),
-                          ),
-                          Text(
-                            'Wed',
-                            style: TextStyle(
-                              fontSize: 15.0,
-                              color: appTheme.primaryColor,
-                              fontWeight: FontWeight.w800,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
           Positioned(
             bottom: 0,
             right: 0,
             child: CustomPaint(
               size: Size(450, (450 * 0.4444444444444444).toDouble()),
               painter: RPSCustomPainter(),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          match.league,
+                          style: TextStyle(
+                            fontSize: 23.0,
+                            color: appTheme.primaryColor,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        Text(match.arena),
+                        SizedBox(height: 10),
+                        Text('24 Feb 2021, 2:00 AM'),
+                        SizedBox(height: 20),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          '24',
+                          style: TextStyle(
+                            fontSize: 23.0,
+                            color: appTheme.primaryColor,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                        Text(
+                          'Wed',
+                          style: TextStyle(
+                            fontSize: 15.0,
+                            color: appTheme.primaryColor,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Image(
+                        image: AssetImage('assets/logos/chelsea-fc.png'),
+                        width: 55,
+                        height: 55,
+                      ),
+                      // SizedBox(width: 10),
+                      Text('Chelsea',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w900,
+                          )),
+                      Text(' - '),
+                      Text('Arsenal',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w900,
+                          )),
+                      // SizedBox(width: 10),
+                      Image(
+                        image: AssetImage('assets/logos/arsenal-fc.png'),
+                        width: 55,
+                        height: 55,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
         ],
